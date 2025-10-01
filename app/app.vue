3e79f0e364ed63a185b7a0d0913466ac168e740a
Rafael Krbashyan <rkrbashyan@teldev.io>
2025-10-01 17:36:24 +0300

    Initial configs with TS, Pinia, Scss, Eslint, Prettier ...

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Nuxt 4</h1>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+h1 {
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
